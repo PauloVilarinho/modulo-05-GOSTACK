b9229d4a4ccfd9193686646a547d1ec489850f50
Paulo Vilarinho <paulotarsobranco@hotmail.com>
2019-10-23 22:28:37 -0300

    feature: listing github repositories

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -23,7 +23,7 @@ export const Container = styled.div`
 export const Form = styled.form`
   margin-top: 30px;
   display: flex;
-  flex-direction: rox;
+  flex-direction: row;
 
   input {
     flex: 1;
@@ -71,4 +71,26 @@ export const SubmitButton = styled.button.attrs(props => ({
         animation: ${rotate} 2s linear infinite;
       }
     `}
+`;
+
+export const List = styled.ul`
+  list-style: none;
+  margin-top: 30px;
+
+  li {
+    padding: 15px 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    & + li {
+      border-top: 1px solid #eee;
+    }
+
+    a {
+      color: #7159c1;
+      text-decoration: none;
+    }
+  }
 `;
